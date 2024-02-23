5e480faec6d95addbb4c6a98274375ddbb9ba5b3
Sak Khakna <sthissakkhakna@gmail.com>
2024-02-23 14:44:09 +0700

    add banner and grid in home page and edit nav and sidebar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import BottomNavigation from "./BottomNavigation";
+import Sidebar from "./Sidebar";
 
 function AppLayout() {
-  return (
-    <div className="flex flex-col xl:flex-row">
-      <aside className="fixed h-screen top-0 left-0 bottom-0 bg-green-600">
-        <div className="w-[230px] p-4 md:p-10 h-screen hidden xl:flex flex-col gap-10">
-          Aside
-        </div>
-      </aside>
-      <main className="w-full xl:ml-[230px]">
-        <Header />
-        <Outlet />
-      </main>
-      <BottomNavigation />
-    </div>
-  );
+	return (
+		<div className="flex flex-col xl:flex-row">
+			<Sidebar />
+			<main className="w-full xl:ml-[230px]">
+				<Header />
+				<Outlet />
+			</main>
+			<BottomNavigation />
+		</div>
+	);
 }
 
 export default AppLayout;
